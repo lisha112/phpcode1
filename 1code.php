12b5bca350f8cb096a79f766ace84729b50796b8
lisha112 <146163320+lisha112@users.noreply.github.com>
2023-09-28 11:49:03 +0500

    Add files via upload

---
+++ 1code.php
@@ -0,0 +1,23 @@
+<?php
+
+$a=12;
+$b=42;
+
+
+echo " Multiplication " .$a. "*" .$b. "="
+.$a*$b;
+echo "<br>";
+
+echo " Sum " .$a."+" .$b. "="
+.$a+$b;
+echo "<br>";
+
+echo " Division " .$a. "/" .$b. "="
+.$a/$b;
+echo "<br>";
+
+echo " Subtraction " .$a. "-" .$b. "="
+.$a-$b;
+echo "<br>";
+
+?>
